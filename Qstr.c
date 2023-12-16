df6fdf5b9f1cceed64b6f2369867db3bdc59309d
NotMe5220 <153383069+Vedant4857@users.noreply.github.com>
2023-12-16 14:46:08 +0530

    Formatted

--- Qstr.c
+++ Qstr.c
@@ -1,48 +1,53 @@
-#include<stdio.h>
+#include <stdio.h>
 #define SIZE 20
-struct book_detail {
-    
+struct book_detail
+{
+
     char book_title[50];
     char author_name[50];
     char publication[50];
     int price;
 };
 
-void output(struct book_detail z[],int n) {
+void output(struct book_detail z[], int n)
+{
 
     int i;
-    for(i = 0;i<n;i++) {
+    for (i = 0; i < n; i++)
+    {
 
-        printf("\n\n\tBook %d details",i+1);
+        printf("\n\n\tBook %d details", i + 1);
 
-        printf("\nBook title:%s",z[i].book_title);
-        printf("\nAuthor name:%s",z[i].author_name);
-        printf("\nPublication:%s",z[i].publication);
-        printf("\nPrice: %d",z[i].price);
+        printf("\nBook title:%s", z[i].book_title);
+        printf("\nAuthor name:%s", z[i].author_name);
+        printf("\nPublication:%s", z[i].publication);
+        printf("\nPrice: %d", z[i].price);
     }
 }
 
-int main() {
+int main()
+{
     struct book_detail b[SIZE];
-    int i ,num;
+    int i, num;
     printf("Enter the number of books:");
-    scanf("%d",&num);
+    scanf("%d", &num);
 
-    for(i = 0;i<num;i++){
+    for (i = 0; i < num; i++)
+    {
 
-        printf("\tENTER BOOK NO.%d DETAILS",i+1);
+        printf("\tENTER BOOK NO.%d DETAILS", i + 1);
 
         printf("\nEnter the book title:");
-        scanf("%s",b[i].book_title);
+        scanf("%s", b[i].book_title);
 
         printf("Enter the auhtor name:");
-        scanf("%s",b[i].author_name);
+        scanf("%s", b[i].author_name);
 
         printf("Enter the publication:");
-        scanf("%s",b[i].publication);
+        scanf("%s", b[i].publication);
 
         printf("Enter the price:");
-        scanf("%d",&b[i].price);
+        scanf("%d", &b[i].price);
     }
-     output(b,num);
+    output(b, num);
 }
